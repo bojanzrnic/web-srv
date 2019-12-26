efcbfcfa095734d691f7114eff8c51e4f0c69a4b
bojanzrnic <zrnic.b@gmail.com>
2019-12-26 16:24:13 +0100

    korigovan i usklađen sa predavanjem

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,30 @@
-console.log('klijent JS fajl učitan');
+console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle')
-    .then((respons) => {
-        respons.json()
-        .then((data) => {
-            console.log(data)
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const poruka1 = document.querySelector('#poruka1')
+const poruka2 = document.querySelector('#poruka2')
+
+//poruka1.textContent = 'poruka1'
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    poruka1.textContent = "Loading..."
+    poruka2.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                poruka1.textContent = data.error
+            } else {
+                poruka1.textContent = data.location
+                poruka2.textContent = data.forecast
+            }
         })
     })
+
+    search.value = ''
+})
--- src/app.js
+++ src/app.js
@@ -1,65 +1,101 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('hbs');
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const app = express();
-// definiši view engine kao "handlebars"
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../templates/views'));
-hbs.registerPartials(path.join(__dirname, '../templates/partials'));
+const app = express()
 
-// putanje do statičkog foldera
-app.use(express.static(path.join(__dirname, '../public')));
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-// definisanje ruta
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-  res.render('index', {
-    title: "Weather Applikac",
-    name: "Bojan Zrnić"
-  })
-});
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    title: "About Weather",
-    name:  "Bojan Zrnić"
-  })
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/weather', (req, res) => {
-  if(!req.query.address) {
-    return res.send({
-      error: "Moraš poslati adresu"
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
-  };
-  res.send({
-    title: "About Weather",
-    name:  "Bojan Zrnić",
-    adress: req.query.address
-  })
-});
+})
 
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    message: "help tehts tsghek doajh aoih ahd a",
-    title: "Help Weather",
-    name:  "Bojan Zrnić"
-  })
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
 })
 
 app.get('*', (req, res) => {
-  res.render('error404', {
-    greska: "Greška na starnici 404"
-  })
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
 })
 
-app.get('/helo/*', (req, res) => {
-  res.render('error404', {
-    greska: "Help tema nije pronađena"
-  })
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server je pokrenut na portu: ${port}`));
