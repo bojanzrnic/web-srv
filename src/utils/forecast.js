8df2690327f05ce0bc824047764d80899c140b61
bojanzrnic <zrnic.b@gmail.com>
2019-12-28 12:32:00 +0100

    daodata min i max temperatura, srpski jezik i celzij

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,18 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/c0a61b6108e81d6b8ba7a3813c22b448/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/c0a61b6108e81d6b8ba7a3813c22b448/' + latitude + ',' + longitude + '?lang=sr&units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Unable to connect to weather service!', undefined)
+            callback('Ne mogu se konektovati na Weather API!', undefined)
         } else if (body.error) {
-            callback('Unable to find location', undefined)
+            callback('Ne mogu pronaći lokaciju.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+//            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' Trenutno je  ' + body.currently.temperature + ' stepeni Cel. vani. Postoji ' + body.currently.precipProbability + '% mogućnosti za kišu.' + 
+                'Maksimalna dnevna temperatura je ' + body.daily.data[0].temperatureHigh + ', a minimalna je ' + body.daily.data[0].temperatureLow +' stepeni Cel.'
+            )
         }
     })
 }
